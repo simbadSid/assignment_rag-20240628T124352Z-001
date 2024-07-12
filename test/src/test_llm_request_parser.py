a540219fa5b2336ea01fb250822fd3a4ffacb0a0
simbadSid <sidlakhdar.riyane@gmail.com>
2024-07-13 00:57:58 +0300

    Fix test_llm_request_parser

--- test/src/test_llm_request_parser.py
+++ test/src/test_llm_request_parser.py
@@ -13,20 +13,17 @@ def llm_request_parser():
     return LlmRequestParser(Config())
 
 
-@pytest.mark.parametrize("user_query, expected_data", [
-    ("What was the total revenue for the company in FY 1990?"                   , "1990"),
-    ("What was the total revenue for the company in FY from 1990 until 2000?"   , "1990 - 2000"),
-    ("What was the total revenue for the company?"                              , "")])
-def test_parse_user_request(llm_request_parser, user_query, expected_data):
+@pytest.mark.parametrize("user_query, expected_data_list", [
+    ("What was the total revenue for the company in FY 1990?"                   , ["1990"]),
+    ("What was the total revenue for the company in FY from 1990 until 2000?"   , ["1990", "2000"]),
+    ("What was the total revenue for the company?"                              , [""])])
+def test_parse_user_request(llm_request_parser, user_query, expected_data_list):
     log("Starting test: parse_user_request", "info")
     llm_request_parser = LlmRequestParser(config)
     query = QueryRequest(company_id=642, query=user_query)
     llm_request_parser.parse_user_request(query)
 
 
-
-    assert \
-        (llm_request_parser.request_context.date == expected_data)\
-        or\
-        (expected_data != "" and expected_data in llm_request_parser.request_context.date)
+    for expected_data in expected_data_list:
+        assert expected_data in llm_request_parser.request_context.date
     log("Completed test: parse_user_request", "info")
